f015cab1e2eb9f6b68a06b4f40391698e25d82aa
Pabodha <tppabasarani2004@gmail.com>
2026-06-10 14:32:00 +0530

    Add search and average calculation

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main { 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
 
         Scanner input = new Scanner(System.in);
         ArrayList<Student> students = new ArrayList<>();
